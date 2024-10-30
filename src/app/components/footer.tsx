fd6773119ab620b9861af63b138b1014ed26d28e
Nida <nnpervez333@gmail.com>
2024-10-30 10:09:30 -0700

    change

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div>
-      <div className="h-[504px] w-full px-40 py-[104px] bg-black flex-col justify-center items-start gap-6 inline-flex max-md:px-5 max-md:py-10">
+      <div className="h-auto w-full px-40 py-[104px] bg-black flex-col justify-center items-start gap-6 inline-flex max-md:px-5 max-md:py-10">
         <div className="self-stretch justify-between items-start inline-flex flex-wrap">
           <div className="flex-col justify-start items-start gap-6 inline-flex max-md:w-full">
             <div className="flex-col justify-start items-center gap-2.5 flex">
@@ -18,47 +18,21 @@ const Footer = () => {
               <div className="text-white text-base font-semibold font-['Inter'] leading-none">
                 Services
               </div>
-              <div className="self-stretch text-[#cfcfcf] text-sm font-normal font-['Inter'] leading-loose">
-                Bonus program
-              </div>
-              <div className="self-stretch text-[#cfcfcf] text-sm font-normal font-['Inter'] leading-loose">
-                Gift cards
-              </div>
-              <div className="self-stretch text-[#cfcfcf] text-sm font-normal font-['Inter'] leading-loose">
-                Credit and payment
-              </div>
-              <div className="self-stretch text-[#cfcfcf] text-sm font-normal font-['Inter'] leading-loose">
-                Service contracts
-              </div>
-              <div className="self-stretch text-[#cfcfcf] text-sm font-normal font-['Inter'] leading-loose">
-                Non-cash account
-              </div>
-              <div className="self-stretch text-[#cfcfcf] text-sm font-normal font-['Inter'] leading-loose">
-                Payment
-              </div>
+              {["Bonus program", "Gift cards", "Credit and payment", "Service contracts", "Non-cash account", "Payment"].map((service, index) => (
+                <div key={index} className="self-stretch text-[#cfcfcf] text-sm font-normal font-['Inter'] leading-loose">
+                  {service}
+                </div>
+              ))}
             </div>
             <div className="w-[295.50px] flex-col justify-start items-start gap-2 inline-flex max-md:w-full">
               <div className="text-white text-base font-semibold font-['Inter'] leading-none">
                 Assistance to the buyer
               </div>
-              <div className="self-stretch text-[#cfcfcf] text-sm font-normal font-['Inter'] leading-loose">
-                Find an order
-              </div>
-              <div className="self-stretch text-[#cfcfcf] text-sm font-normal font-['Inter'] leading-loose">
-                Terms of delivery
-              </div>
-              <div className="self-stretch text-[#cfcfcf] text-sm font-normal font-['Inter'] leading-loose">
-                Exchange and return of goods
-              </div>
-              <div className="self-stretch text-[#cfcfcf] text-sm font-normal font-['Inter'] leading-loose">
-                Guarantee
-              </div>
-              <div className="self-stretch text-[#cfcfcf] text-sm font-normal font-['Inter'] leading-loose">
-                Frequently asked questions
-              </div>
-              <div className="self-stretch text-[#cfcfcf] text-sm font-normal font-['Inter'] leading-loose">
-                Terms of use of the site
-              </div>
+              {["Find an order", "Terms of delivery", "Exchange and return of goods", "Guarantee", "Frequently asked questions", "Terms of use of the site"].map((assist, index) => (
+                <div key={index} className="self-stretch text-[#cfcfcf] text-sm font-normal font-['Inter'] leading-loose">
+                  {assist}
+                </div>
+              ))}
             </div>
           </div>
         </div>
